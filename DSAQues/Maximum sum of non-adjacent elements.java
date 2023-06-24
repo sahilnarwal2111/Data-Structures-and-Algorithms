0c20e7a36bbe2c17f1504749628abea126b3ce4f
Sahil Narwal <109059714+sahilnarwal2111@users.noreply.github.com>
2023-06-24 17:31:08 +0530

    Create Maximum sum of non-adjacent elements.java
    YouTube : https://www.youtube.com/watch?v=GrMBfJNk_NY&list=PLgUwDviBIf0qUlt5H_kiKYaNSqJ81PMMY&index=6

---
+++ DSAQues/Maximum sum of non-adjacent elements.java
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h> 
+int f(int i , vector<int> &nums, vector<int>&dp){
+    if(i == 0) return nums[i];
+    if(i<0) return 0;
+    if(dp[i]!=-1) return dp[i];
+    int pick = nums[i] + f(i-2,nums,dp);
+    int notPick = 0 + f(i-1, nums,dp);
+    return dp[i]= max(pick, notPick);
+
+}
+int maximumNonAdjacentSum(vector<int> &nums){
+    // Write your code here.
+    vector<int> dp (nums.size(), -1);
+    return f(nums.size()-1, nums,dp);
+}
+
+-------------Tabulation--------------------
+  int maximumNonAdjacentSum(vector<int> &nums){
+    // Write your code here.
+    vector<int> dp (nums.size(), -1);
+    //Tabulation
+    dp[0] = nums[0];
+    int neg = 0;
+    for(int i = 1; i < nums.size(); i++){
+        int pick = nums[i];
+        if(i-2>=0) pick += dp[i-2];
+        int notPick = dp[i-1];
+        dp[i] = max(pick, notPick);
+    }
+    return dp[nums.size()-1];
+------------Space Optimization---------------
+  int prev = nums[0];
+    int prev2 = 0;
+    for(int i = 1 ; i < nums.size(); i++){
+        int pick = nums[i] + prev2;
+        int notPick = prev;
+        int curi = max(pick, notPick);
+        prev2 = prev;
+        prev = curi;
+    }
+    return prev;
